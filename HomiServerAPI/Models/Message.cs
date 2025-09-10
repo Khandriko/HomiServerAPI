f4762d253fd08599d138d1578e28d9b4b7ab8020
Максим <tvoj.email@example.com>
2025-09-10 16:00:38 +0200

    Добавлены модели User и Message

---
+++ HomiServerAPI/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace HomiServerAPI.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
---
+++ HomiServerAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace HomiServerAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; } // Хранить хеш пароля
+    }
+}
